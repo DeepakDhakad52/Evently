6ab77afa5be654e06661d7a761e9b000104422ca
Deepak Nagar <ddhakad745@gmail.com>
2024-01-03 11:55:58 +0530

    Implement Stripe

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
-// import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 import { IEvent } from '@/lib/database/models/event.model';
 import { Button } from '../ui/button';
-// import { checkoutOrder } from '@/lib/actions/order.actions';
+import { checkoutOrder } from '@/lib/actions/order.actions';
 
-// loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   useEffect(() => {
@@ -24,12 +24,12 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
     const order = {
       eventTitle: event.title,
       eventId: event._id,
-      price: event.price,
+      price: event.price!,
       isFree: event.isFree,
       buyerId: userId
     }
 
-    // await checkoutOrder(order);
+    await checkoutOrder(order);
   }
 
   return (
